5d572d686610f9cfad8b8007ddd1835adaa4b8a6
Yuri Mello <58647179+YuriMelloFiles@users.noreply.github.com>
2022-04-28 22:27:20 -0300

    Trabalhando nos comentários
    Construindo a parte de comentários do site.

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-alert('to aqui')
+/* abre e fecha o menu ao clicar no icone */
+
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* quando clicar em um item do menu, esconder o menu */
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* mudar o header da página quando der scroll */
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    /* scroll > header */
+    header.classList.add('scroll')
+  } else {
+    /* scroll < header*/
+    header.classList.remove('scroll')
+  }
+})
